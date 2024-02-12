354cf2298112b6616562d1b73c88134ec358ce06
Skycoder42 <Skycoder42@users.noreply.github.com>
2024-02-12 12:24:15 +0000

    cleanup project

--- test/apple_fotos_xmp_fixup_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:apple_fotos_xmp_fixup/apple_fotos_xmp_fixup.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
